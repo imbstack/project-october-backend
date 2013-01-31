a491580444ec0fd4a177db557864895387970cf6
Brian Stack <bis12@case.edu>
2013-01-31 18:15:12 -0500

    Get server working

--- src/main/scala/october.scala
+++ src/main/scala/october.scala
@@ -1,3 +1,44 @@
-object Hi {
-    def main(args: Array[String]) = println("Hi!")
+import org.apache.thrift.transport.TServerSocket
+import org.apache.thrift.server.TSimpleServer
+import org.apache.thrift.server.TServer.Args
+import org.apache.thrift.protocol.TBinaryProtocol
+import java.net.InetSocketAddress
+import com.twitter.finagle.builder.ServerBuilder
+import com.twitter.finagle.thrift.ThriftServerFramedCodec
+import com.twitter.util._
+
+
+import october.Post
+import october.PostList
+import october.Recommender
+
+class RecHandler extends october.Recommender.FutureIface {
+    override def ping(): Future[String] = Future.value("Pong")
+
+    override def recPosts(userId: Long): Future[PostList] = Future.value(new SPostList(Option(0.5), Seq()))
+
+}
+
+class SPostList(val confidence: Option[Double],
+                val posts: Seq[SPost]) extends october.PostList {
+}
+
+class SPost(val weight: Option[Double],
+            val postId: Long) extends october.Post {
+}
+
+object RecServer {
+    def main(args: Array[String]) {
+        val protocol = new TBinaryProtocol.Factory()
+        val handler = new RecHandler()
+        val service = new Recommender.FinagledService(handler, protocol)
+        val address = new InetSocketAddress("127.0.0.1", 9090) // TODO: config this
+        println("Server going up!")
+        var builder = ServerBuilder()
+            .codec(ThriftServerFramedCodec())
+            .name("recommender_services")
+            .bindTo(address)
+            .build(service)
+         println("Press Ctrl+C to exit")
+    }
 }
